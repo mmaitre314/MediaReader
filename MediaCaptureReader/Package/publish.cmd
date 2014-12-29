ee86540b52d6d1f230e78dd3cef4142edc9d1559
mmaitre314 <mmaitre314@users.noreply.github.com>
2014-12-28 23:59:32 -0800

    Fix NuGet.exe path

--- MediaCaptureReader/Package/publish.cmd
+++ MediaCaptureReader/Package/publish.cmd
@@ -5,5 +5,5 @@ set VERSION=1.0.2
 
 set OUTPUT=c:\NuGet\
 
-nuget push %OUTPUT%Packages\MMaitre.MediaCaptureReader.%VERSION%.nupkg
-nuget push %OUTPUT%Symbols\MMaitre.MediaCaptureReader.Symbols.%VERSION%.nupkg -Source http://nuget.gw.symbolsource.org/Public/NuGet 
+%OUTPUT%nuget push %OUTPUT%Packages\MMaitre.MediaCaptureReader.%VERSION%.nupkg
+%OUTPUT%nuget push %OUTPUT%Symbols\MMaitre.MediaCaptureReader.Symbols.%VERSION%.nupkg -Source http://nuget.gw.symbolsource.org/Public/NuGet 
